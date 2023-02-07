8b805fba5c9265e78f4a4177db343480337fa3bb
Chinwenma <okoriechinwe1327@gmail.com>
2023-02-07 14:54:24 -0800

    ok

--- src/App.js
+++ src/App.js
@@ -2,60 +2,21 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Card from './components/Card';
-import dev from './images/dev.png';
+import Contact from './components/Contact';
 
 import './App.css';
 
 export default function App() {
   return (
     <div>
+     <Contact />
+     <Contact />
+     <Contact />
+     <Contact />
 
-<div className='w-[175px] font-popins mt-6 bg-slate-600'>
-            <img src={dev} alt=""  className='w[100%] rounded-full border-2'/>
-            <div>
-                <span>5.0</span>
-                <span>(6) .</span>
-                <span>
-                    USA
-                </span>
-            </div>
-            <p>I am Coming Home</p>
-            <p>
-               From $136 / person 
-            </p>
-        </div>
-        <div className='w-[175px] font-popins mt-6 bg-slate-600'>
-            <img src={dev} alt=""  className='w[100%] rounded-full border-2'/>
-            <div>
-                <span>5.0</span>
-                <span>(6) .</span>
-                <span>
-                    USA
-                </span>
-            </div>
-            <p>I am Coming Home</p>
-            <p>
-               From $136 / person 
-            </p>
-        </div>
-        <div className='w-[175px] font-popins mt-6 bg-slate-600'>
-            <img src={dev} alt=""  className='w[100%] rounded-full border-2'/>
-            <div>
-                <span>5.0</span>
-                <span>(6) .</span>
-                <span>
-                    USA
-                </span>
-            </div>
-            <p>I am Coming Home</p>
-            <p>
-               From $136 / person 
-            </p>
-        </div>
      <Navbar />
      <Hero />
      <Card />
     </div>
   );
 }
-
---
+++ src/components/Contact.jsx
@@ -0,0 +1,19 @@
+import dev from '../images/dev.png';
+export default function Contact(){
+    return (
+        <div className=' w-[175px] font-popins mt-6 bg-slate-600'>
+            <img src={dev} alt=""  className='w[100%] rounded-full border-2'/>
+            <div>
+                <span>5.0</span>
+                <span>(6) .</span>
+                <span>
+                    USA
+                </span>
+            </div>
+            <p>I am Coming Home</p>
+            <p>
+               From $136 / person 
+            </p>
+        </div>
+    )
+}
